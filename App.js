31aedf2f3f0bdbf92f9de9ee02722d7b3c0e936a
san480 <92041106+san480@users.noreply.github.com>
2021-10-31 10:36:06 -1200

    Add files via upload

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login}/>
         <Stack.Screen name="Home" component={Home}/>
 
       </Stack.Navigator>
