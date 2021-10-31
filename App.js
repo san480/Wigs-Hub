9e011d4fa40e195859fdf798003e97ea35defaae
san480 <92041106+san480@users.noreply.github.com>
2021-10-31 10:29:17 -1200

    Add files via upload

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { createNativeStackNavigator} from "@react-navigation/native-stack";
 import {NavigationContainer} from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
-import login from "./screens/login"
+import Login from "./screens/Login"
 import Home from "./screens/Home"
 
 const Stack = createNativeStackNavigator();
@@ -12,7 +12,6 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="login" component={login}/>
         <Stack.Screen name="Home" component={Home}/>
 
       </Stack.Navigator>
--- Login.js
+++ Login.js
@@ -1,53 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text,TextInput,Image, View, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { Button, StyleSheet, Text,TextInput,Image, View, TouchableOpacity, TouchableHighlight, ImageBackground } from 'react-native';
 
 export default function Login({navigation}) {
   return (
-    <View style={styles.container}>
-      <TouchableHighlight onPress={() => console.log('clicked')}>
-      <Image style={{height:170}} source={require("./assets/welcome.png")}/>
 
-      </TouchableHighlight>
+    <ImageBackground
+      style={styles.container}
+      resizeMode = "contain"
+      source={require('./assets/background.jpg')}>
 
-        
 
-      <View style={{marginTop:15,alignContent:'flex-end'}}>
-        <Text style ={{fontSize: 18}}>username or email</Text>
-      </View>
-      <View style={{        
-        borderRadius: 28,
-        backgroundColor:"#f2f5f4"}}>
-        <TextInput style={{
-          padding: 20,
-          width:370
-        }}
-        placeholder="username or email"
-        />
-      </View>
-      <View style={{alignItems:'flex-start',marginTop:15}}>
-        <Text style ={{fontSize: 18}}>password</Text>
-      </View>
-      <View style={{        
-        borderRadius: 28,
-        backgroundColor:"#f2f5f4"}}>
-        <TextInput style={{
-          padding: 20,
-          width:370
-        }}
-        placeholder="password"
-        secureTextEntry= {true}
-        />
-      </View>
-      
-      <View style={{width:370, marginTop:20}}>
-        <Button
-        onPress={()=>navigation.navigate('Home')}
-        color='#000'
-        title ='sign in'/>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+
+<View style={{        
+  borderRadius: 28,
+  backgroundColor:"#f2f5f4"}}>
+  <TextInput style={{
+    padding: 20,
+    width:370,
+    
+  }}
+  placeholder="username or email"
+  />
+</View>
+<View style={{        
+  borderRadius: 28,
+  marginTop:35,
+  backgroundColor:"#f2f5f4"}}>
+  <TextInput style={{
+    padding: 20,
+    width:370
+  }}
+  placeholder="password"
+  secureTextEntry= {true}
+  />
+</View>
+
+<View style={{width:370, marginTop:35}}>
+  <Button
+  onPress={()=>navigation.navigate('Home')}
+  color='#000'
+  title ='sign in'/>
+</View>
+<StatusBar style="auto" />
+    </ImageBackground> 
+
+
+   
   );
 }
 
