b6a46af5f1d570ad0cb60aa89bd2ca2356b44bcb
san480 <92041106+san480@users.noreply.github.com>
2021-11-04 01:32:32 -1200

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,FlatList, View, Image } from 'react-native';
 
-export default function Home(navigation) {
+
+export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <View style={{flexDirection:'row',alignContent:'space between'}}>
+      <View>
+        <Image style={{height:200, width:300, marginRight:20}} source={require('./assets/bagss2.jpg')}/>
+        <Text style={{fontSize:30}}>Bags</Text>
+      </View>
+      <View>
+        <Image style={{height:200, width:300}} source={require('./assets/accessories 1.jpg')}/>
+        <Text style={{fontSize:30}}>Accessories</Text>
+      </View>
+    </View> 
+     <View style={{flexDirection:'row',alignContent:'space between'}}>
+      <View>
+        <Image style={{height:200, width:300, marginRight:20}} source={require('./assets/makeupkit1.jpg')}/>
+        <Text style={{fontSize:30}}>Makeupkits</Text>
+      </View>
+      <View>
+        <Image style={{height:200, width:300}} source={require('./assets/perfumes1.jpg')}/>
+        <Text style={{fontSize:30}}>Perfumes</Text>
+      </View>
+    </View> 
       <StatusBar style="auto" />
     </View>
   );
